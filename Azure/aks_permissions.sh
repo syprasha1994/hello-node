736f8f9c668667590a60e350184a8ea890db0218
syprasha1994 <61225226+syprasha1994@users.noreply.github.com>
2020-03-09 16:17:43 +0530

    Update aks_permissions.sh

--- Azure/aks_permissions.sh
+++ Azure/aks_permissions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-AKS_RESOURCE_GROUP=myAKSCluster
-AKS_CLUSTER_NAME=myAKSCluster
-ACR_RESOURCE_GROUP=myACRGroup
-ACR_NAME=myCR001
+AKS_RESOURCE_GROUP=testrg
+AKS_CLUSTER_NAME=testkube
+ACR_RESOURCE_GROUP=testrg
+ACR_NAME=sateco2
 
 # Get the id of the service principal configured for AKS
 CLIENT_ID=$(az aks show --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --query "servicePrincipalProfile.clientId" --output tsv)
